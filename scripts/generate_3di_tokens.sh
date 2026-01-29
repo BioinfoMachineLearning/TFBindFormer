1b2fae64e3e8a51f655535e5802797cc76f7ece8
Ping Liu <149427816+appleliu200807@users.noreply.github.com>
2026-01-29 13:06:07 -0600

    Fix quoting and add output message in script
    Updated script to handle quotes and added success message.

--- scripts/generate_3di_tokens.sh
+++ scripts/generate_3di_tokens.sh
@@ -2,17 +2,15 @@
 
 # Generate 3Di tokens from TF PDB structures using Foldseek
 
+set -e  # exit on error (recommended)
+
 PDB_DIR=$1
 OUT_DIR=$2
 
-mkdir -p ${OUT_DIR}
+mkdir -p "${OUT_DIR}"
 
-foldseek createdb -v 3 ${PDB_DIR} ${OUT_DIR}/pdb_3Di
-foldseek lndb ${OUT_DIR}/pdb_3Di_h ${OUT_DIR}/pdb_3Di_ss_h
-foldseek convert2fasta ${OUT_DIR}/pdb_3Di_ss ${OUT_DIR}/pdb_3Di_ss.fasta
+foldseek createdb -v 3 "${PDB_DIR}" "${OUT_DIR}/pdb_3Di"
+foldseek lndb "${OUT_DIR}/pdb_3Di_h" "${OUT_DIR}/pdb_3Di_ss_h"
+foldseek convert2fasta "${OUT_DIR}/pdb_3Di_ss" "${OUT_DIR}/pdb_3Di_ss.fasta"
 
-
-"""
-chmod +x scripts/generate_3di_tokens.sh 
-./generate_3di_tokens.sh pdb_dir output_dir
-"""
+echo "3Di FASTA written to ${OUT_DIR}/pdb_3Di_ss.fasta"
